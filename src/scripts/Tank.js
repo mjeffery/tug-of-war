e6ce74efe808713f88119e829da26e5132d559f2
mjeffery <michael.jeffery@gmail.com>
2014-06-16 01:07:33 -0400

    sword animation

--- src/scripts/Tank.js
+++ src/scripts/Tank.js
@@ -15,17 +15,33 @@
 		this.team = team;
 		this.facing = (team === Team.PLAYER) ? Phaser.RIGHT : Phaser.LEFT;
 
-		this.walk();
 		this.hp.setTo(50);
 		this.hp.x = -32;
 		this.hp.y = -32;
+
+		// Add a sword sprite
+		key = (team == Team.PLAYER) ? 'placeholder-player-sword' : 'placeholder-enemy-sword';
+		var sword = this.sword = game.make.sprite(0, 50, key);
+		this.addChild(sword);
+		sword.anchor.setTo(0.5, 1.0);
+		sword.visible = false;
+
+		this.walk();
+		this.attack();
 	}
 
 	_.extend(Tank, {
 		Walk: { Speed: 100 },
+		Attack: { 
+			Angle: 135,
+			Duration: 600,
+			Easing: Phaser.Easing.Quartic.Out
+		},
 		preload: function(load) {
 			load.spritesheet('placeholder-tank.png', 'assets/spritesheet/placeholder tank.png', 78, 150);
 			load.spritesheet('placeholder-enemy-tank.png', 'assets/spritesheet/placeholder enemy tank.png', 78, 150);
+			load.image('placeholder-player-sword', 'assets/img/placeholder sword.png');
+			load.image('placeholder-enemy-sword', 'assets/img/placeholder enemy sword.png');
 		}
 	});
 
@@ -36,6 +52,7 @@
 
 	_.extend(Tank.prototype, {
 		think: function() {
+			
 		},
 		walk: function() {
 			this.animations.play('walk');
@@ -46,6 +63,23 @@
 				this.body.velocity.x = -Tank.Walk.Speed;
 
 			this.state = 'walking';
+		},
+		attack: function() {
+			var sword = this.sword,
+				tween = this.game.add.tween(sword),
+				angle = (this.team == Team.PLAYER) ? Tank.Attack.Angle : -Tank.Attack.Angle;
+
+			sword.visible = true;
+
+			tween.to({ angle: angle }, Tank.Attack.Duration, Tank.Attack.Easing)
+				.onComplete.addOnce(function() {
+					sword.visible = false;
+					sword.angle = 0;
+					this.attack();
+					//TODO damage
+				}, this);
+
+			tween.start();
 		}
 	});
 
